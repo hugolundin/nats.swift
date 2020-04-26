cce48733210ef5b7c0e3123d4054cf28e6693b22
Hugo Lundin <hugo@hugolundin.se>
2020-04-26 18:06:19 +0200

    Start working on parser supporting partial reads

--- Tests/nats.swiftTests/ParserTests.swift
+++ Tests/nats.swiftTests/ParserTests.swift
@@ -1,8 +1,23 @@
 //
 //  ParserTests.swift
-//  nats.swift
 //
-//  Created by Hugo Lundin on 2020-04-19.
+//
+//  Created by Hugo Lundin on 2020-04-14.
 //
 
+import XCTest
 import Foundation
+@testable import nats_swift
+
+final class ParserTests: XCTestCase {
+    
+    let parser = Parser2()
+    
+    func testMessage() {
+        
+    }
+    
+    static var allTests = [
+        ("testMessage", testMessage)
+    ]
+}
